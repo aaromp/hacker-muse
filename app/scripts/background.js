559a8e566d4a6e41b59e826f6a63d2d45db1adc3
John Doty <jrhdoty@gmail.com>
2014-10-10 12:32:54 -0700

    update background to track score of a specific post

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,6 +4,13 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+var fb = new Firebase('https://hacker-news.firebaseio.com/v0/item');
+var post = fb.child('8420948');
+
+post.on('value', function(data){
+  console.log('inside callback to post');
+  console.log('data is: ', data.val().score);
+  chrome.browserAction.setBadgeText({text: data.val().score.toString(10)});
+});
